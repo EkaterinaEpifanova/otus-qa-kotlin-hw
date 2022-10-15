9d57f797366c18c08359fc30cc2498c0d40f7839
Ekaterina Epifanova <eepifanova@samokat.ru>
2022-10-15 15:45:03 +0300

    Реализованы тесты для варианта Material Components

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,23 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
-    kotlin("jvm") version "1.7.10"
+    val kotlinVersion = "1.7.10"
+    id("com.android.application") version ("7.3.0-rc01") apply (false)
+    id("org.jetbrains.kotlin.android") version kotlinVersion apply (false)
+    id("org.jetbrains.kotlin.kapt") version kotlinVersion apply (false)
+    id("org.jetbrains.kotlin.android.extensions") version kotlinVersion apply (false)
+    id("org.jetbrains.kotlin.multiplatform") version kotlinVersion apply (false)
+    id("org.jlleitschuh.gradle.ktlint") version "10.3.0"
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
-
-repositories {
-    mavenCentral()
+apply {
+    from("translators.gradle.kts")
 }
 
-dependencies {
-    testImplementation(kotlin("test"))
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven(url = "https://plugins.gradle.org/m2/")
+        maven(url = "https://oss.sonatype.org/content/repositories/snapshots/")
+        maven(url = "https://jitpack.io")
+    }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,13 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+    }
+    resolutionStrategy.eachPlugin {
+        if (requested.id.id == "com.android.application") {
+            useModule("com.android.tools.build:gradle:${requested.version}")
+        }
+    }
+}
 
-rootProject.name = "otus-qa-kotlin-hw"
-
+include(":uhabits-android", ":uhabits-core", ":uhabits-server")
